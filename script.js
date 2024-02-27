04111846700a11e8c0f3ecf5b65c1d67c753af31
Eliza Sousa <eli.sinais@gmail.com>
2024-02-27 13:42:57 -0300

    Fluxo de exceções implementadas à  função calculateSum

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 function calculateSum() {
-  const value1 = document.getElementById('value1').value;
-  const value2 = document.getElementById('value2').value;
-  const result = Number(value1) + Number(value2);
+  const value1 = Number(document.getElementById('value1').value);
+  const value2 = Number(document.getElementById('value2').value);
+  if (value1 <= 0 || value2 <= 0) {
+    throw new Error('Preencha os campos para realizar a soma');
+  }
+  if (Number.isNaN(value1) || Number.isNaN(value2)) {
+    throw new Error('Informe dois números para realizar a soma');
+  }
+  const result = value1 + value2;
 
   return result;
 }
@@ -11,8 +17,17 @@ function displayResult(result) {
 }
 
 function sum() {
-  const result = calculateSum();
-  displayResult(result);
+  let result;
+  try {
+    result = calculateSum();
+  } catch (error) {
+    result = error.message;
+    return result;
+  } finally {
+    displayResult(result);
+    document.getElementById('value1').value = '';
+    document.getElementById('value2').value = '';
+  }
 }
 
 window.onload = () => {
